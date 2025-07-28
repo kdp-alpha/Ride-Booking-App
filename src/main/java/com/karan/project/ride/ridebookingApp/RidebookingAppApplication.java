aaa60d66bed210db9807367d7bfc528f8f0e1cb1
Karan Dinkar Pawar <karandinkarpawar@Mac.lan>
2025-07-28 22:52:13 +0530

    ride bookng app initialized in spring boot

---
+++ src/main/java/com/karan/project/ride/ridebookingApp/RidebookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.karan.project.ride.ridebookingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidebookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidebookingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karan/project/ride/ridebookingApp/RidebookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karan.project.ride.ridebookingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidebookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
